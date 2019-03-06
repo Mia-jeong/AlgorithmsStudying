2e18fcf845d4e083ac69f86123bf010b272e0916
Mia-jung <miajeongdev@gmail.com>
2019-03-06 19:03:19 +0900

    solve permutation using recursion method

--- Recursion/src/Permutation.java
+++ Recursion/src/Permutation.java
@@ -1,13 +1,52 @@
 import java.util.Scanner;
 
+/*
+<input>
+4 2
+<output>
+ab
+ac
+ad
+ba
+bc
+bd
+ca
+cb
+cd
+da
+db
+dc
+*/
 public class Permutation {
 
 	static boolean flags[];
-	public static void main(String[] args) {
+	static int n;
+	public static void main(String[] ars) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int r = sc.nextInt();
+		n = sc.nextInt();  
+		int r = sc.nextInt();  
 		flags = new boolean[n];
+		
+		permutation(r, "");
+		
+	}
+	
+	public static void permutation (int idx, String sb) {
+		if(idx > 0) {
+			for (int i = 0; i < flags.length; i++) {
+				if(flags[i] == false) {
+					flags[i] = true;
+					String temp = (char)(i+97)+"";
+					permutation(idx-1, sb+temp);
+					flags[i] = false;
+				}
+			}
+		}
+		else {
+			System.out.print(sb);
+			System.out.println();
+		}
+		
 	}
 
 }
